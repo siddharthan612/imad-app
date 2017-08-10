8f72075a68048b8918a00eb2f10515faa12779f5
siddharthan612 <30855865+siddharthan612@users.noreply.github.com>
2017-08-11 01:08:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log('Loaded!');
 
 var element = document.getElementById('main-text');
-element.innerHTML='New Value';
+element.innerHTML = 'New Value';
 
-var img=document.getElementById('madi');
-img.onclick=function(){
-    img.style.marginLeft= '100px';
+var img = document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
 }
